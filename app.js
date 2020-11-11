b26b1f1ddd82c41f1e6ae776ad7a574e014c93f8
pollitaPam <pgonzalez29@ucol.mx>
2020-11-11 01:31:44 -0600

    0.24

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(express.static("./public"))
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/front/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 
 });
 
 app.get('/pagina2', (req, res) =>{
-    res.sendFile(__dirname + '/front/pagina2.html');
+    res.sendFile(__dirname + '/public/pagina2.html');
 });
 
 app.get('/pagina3', (req, res) => {
-    res.sendFile(__dirname + '/front/pagina3.html')
+    res.sendFile(__dirname + '/public/pagina3.html');
+});
+
+app.get('/pagina4', (req, res) => {
+    res.sendFile(__dirname + '/public/pagina4.html');
 });
 
 app.listen(port, () => {
